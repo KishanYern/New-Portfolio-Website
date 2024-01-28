0c943b5df8b96258eb1908c4228c4a6573f1bbd8
Kishan Yerneni <kishanyerneni@gmail.com>
2024-01-27 23:36:08 -0600

    fixed overlap problem with md

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,42 +11,42 @@ const Home = () => {
                 <div
                     className='absolute top-24 left-24 w-80 h-80 opacity-70 
             rounded-full bg-yellow-100 mix-blend-multiply 
-            filter blur-xl animate-blob z-0'
+            filter blur-xl animate-blob z-0 pointer-events-none'
                 />
 
                 <div
                     className='absolute bottom-40 right-36 w-80 h-80 opacity-70 
             rounded-full bg-red-200 mix-blend-multiply 
-            filter blur-xl animate-blob animation-delay-2 z-0'
+            filter blur-xl animate-blob animation-delay-2 z-0 pointer-events-none'
                 />
 
                 <div
                     className='absolute bottom-52 left-36 w-80 h-80 opacity-70 
             rounded-full bg-blue-200 mix-blend-multiply 
-            filter blur-xl animate-blob animation-delay-4 z-0'
+            filter blur-xl animate-blob animation-delay-4 z-0 pointer-events-none'
                 />
 
                 <div
                     className='absolute top-80 right-2/4 w-80 h-80 opacity-70 
             rounded-full bg-green-200 mix-blend-multiply 
-            filter blur-xl animate-blob animation-delay-6 z-0'
+            filter blur-xl animate-blob animation-delay-6 z-0 pointer-events-none'
                 />
 
                 <div
                     className='absolute bottom-2/4 right-1/4 w-80 h-80 opacity-70 
             rounded-full bg-gray-300 mix-blend-multiply 
-            filter blur-xl animate-blob z-0'
+            filter blur-xl animate-blob z-0 pointer-events-none'
                 />
 
                 <div
                     className='absolute top-1/4 right-1/4 w-80 h-80 opacity-70 
             rounded-full bg-orange-200 mix-blend-multiply 
-            filter blur-xl animate-blob animation-delay-6 z-0'
+            filter blur-xl animate-blob animation-delay-6 z-0 pointer-events-none'
                 />
             </div>
 
             <div className='z-10 mx-10 flex flex-col items-center justify-center h-full w-full px-4 md:flex-row'>
-                <div className='text-9xl pr-56 ml-0 pl-0 font-bold bg-gradient-to-b from-red-700 to-red-400 bg-clip-text text-transparent'>
+                <div className='text-9xl pr-56 ml-0 pl-0 font-bold bg-gradient-to-b w-1/2 from-red-700 to-red-400 bg-clip-text text-transparent'>
                     Kishan Yerneni
                 </div>
                 <div>
@@ -54,6 +54,7 @@ const Home = () => {
                     <GiAirplaneDeparture className='h-52 w-52 ml-20' />{' '}
                 </div>
             </div>
+            <div></div>
         </div>
     );
 };
