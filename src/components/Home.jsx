32438422ba1983ea0f13e079bc44683ffc9d8906
Kishan Yerneni <kishanyerneni@gmail.com>
2024-01-27 23:04:50 -0600

    home page finished

---
+++ src/components/Home.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { GiAirplaneDeparture } from 'react-icons/gi';
+
+const Home = () => {
+    return (
+        <div
+            name='homePage'
+            className='h-screen w-full bg-gradient-to-b from-white to-blue-100 overflow-hidden max-w-screen-ms'
+        >
+            <div>
+                <div
+                    className='absolute top-24 left-24 w-80 h-80 opacity-70 
+            rounded-full bg-yellow-100 mix-blend-multiply 
+            filter blur-xl animate-blob z-0'
+                />
+
+                <div
+                    className='absolute bottom-40 right-36 w-80 h-80 opacity-70 
+            rounded-full bg-red-200 mix-blend-multiply 
+            filter blur-xl animate-blob animation-delay-2 z-0'
+                />
+
+                <div
+                    className='absolute bottom-52 left-36 w-80 h-80 opacity-70 
+            rounded-full bg-blue-200 mix-blend-multiply 
+            filter blur-xl animate-blob animation-delay-4 z-0'
+                />
+
+                <div
+                    className='absolute top-80 right-2/4 w-80 h-80 opacity-70 
+            rounded-full bg-green-200 mix-blend-multiply 
+            filter blur-xl animate-blob animation-delay-6 z-0'
+                />
+
+                <div
+                    className='absolute bottom-2/4 right-1/4 w-80 h-80 opacity-70 
+            rounded-full bg-gray-300 mix-blend-multiply 
+            filter blur-xl animate-blob z-0'
+                />
+
+                <div
+                    className='absolute top-1/4 right-1/4 w-80 h-80 opacity-70 
+            rounded-full bg-orange-200 mix-blend-multiply 
+            filter blur-xl animate-blob animation-delay-6 z-0'
+                />
+            </div>
+
+            <div className='z-10 mx-10 flex flex-col items-center justify-center h-full w-full px-4 md:flex-row'>
+                <div className='text-9xl pr-56 ml-0 pl-0 font-bold bg-gradient-to-b from-red-700 to-red-400 bg-clip-text text-transparent'>
+                    Kishan Yerneni
+                </div>
+                <div>
+                    {' '}
+                    <GiAirplaneDeparture className='h-52 w-52 ml-20' />{' '}
+                </div>
+            </div>
+        </div>
+    );
+};
+export default Home;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -13,7 +13,7 @@ const NavBar = () => {
         },
         {
             id: 3,
-            link: 'portfolio',
+            link: 'projects',
         },
         {
             id: 4,
@@ -28,21 +28,20 @@ const NavBar = () => {
     const [nav, setNav] = useState(false);
 
     return (
-        <div className='flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed'>
+        <div className='flex justify-between items-center w-full h-20 px-4 bg-red fixed'>
             <div>
-                <h1 className='text-5xl font-signature ml-2'>
-                    Kishan's Portfolio Website
-                </h1>
+                <h1 className='text-5xl font-signature ml-2'>Kishan Yerneni</h1>
             </div>
             <ul className='hidden md:flex'>
-                {links.map((link) => {
+                {links.map(({ id, link }) => {
                     return (
                         <li
-                            key={link.id}
-                            className='px-4 cursor-pointer font-medium capitalize text-gray-500 hover:scale-105 duration-200'
+                            key={id}
+                            className='px-4 cursor-pointer font-medium capitalize text-black-500 
+                            hover:scale-105 duration-200 hover:text-red-500 z-10'
                         >
                             {' '}
-                            {link.link}{' '}
+                            {link}{' '}
                         </li>
                     );
                 })}
@@ -59,13 +58,13 @@ const NavBar = () => {
                 <ul
                     className=' flex flex-col justify-center items-center 
             absolute top-0 left-0 w-full h-screen bg-gradient-to-b
-             from-black to-gray-800 text-gray-500'
+             from-red-400 to-white text-gray-500'
                 >
                     {links.map(({ id, link }) => {
                         return (
                             <li
                                 key={id}
-                                className='px-4 cursor-pointer capitalize py-6 text-4xl'
+                                className='px-4 z-10 cursor-pointer capitalize py-6 text-4xl hover:scale-105 duration-200 hover:text-black-500'
                             >
                                 {' '}
                                 {link}{' '}
