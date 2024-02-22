819c122da1307c6ac15899fdd623493c3ade37d7
Kishan Yerneni <kishanyerneni@gmail.com>
2024-02-21 19:13:51 -0600

    added email functionality

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -1,12 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import emailjs from '@emailjs/browser';
 
 const ContactMe = () => {
+    const form = useRef();
+
+    useEffect(() => {
+        emailjs.init({
+            publicKey: 'M7xzawvUKQxVHqu4E',
+            // Do not allow headless browsers
+            blockHeadless: true,
+        });
+    }, []);
+
     const sendEmail = async (e) => {
         e.preventDefault();
-        console.log('sending email');
+
+        setLoading(true);
+
+        const data = {
+            name: e.target[0].value,
+            email: e.target[1].value,
+            message: e.target[2].value,
+        };
+
+        try {
+            emailjs.send('service_8470cys', 'template_6ao15re', data);
+
+            setSuccess(true);
+            setLoading(false);
+            setTimeout(() => {
+                setSuccess(false);
+            }, 3000);
+            form.current.reset();
+        } catch (err) {
+            setLoading(false);
+            console.log(err);
+        }
     };
 
-    const [error, setError] = useState(false);
     const [success, setSuccess] = useState(false);
     const [loading, setLoading] = useState(false);
 
@@ -18,7 +49,7 @@ const ContactMe = () => {
                     className='bg-violet-400 text-white rounded-md p-2 m-2 w-40 h-10 shadow-[0px_10px_1px_rgba(221,_221,_221,_1),_0_10px_20px_rgba(204,_204,_204,_1)] 
                     active:shadow-none active:translate-y-1 hover:scale-105 hover:bg-violet-500'
                 >
-                    Loading
+                    Loading...
                 </button>
             );
         } else if (success) {
@@ -46,15 +77,15 @@ const ContactMe = () => {
 
     return (
         <section
-            className='h-[100vh] bg-gradient-to-b from-red-50 to-white'
+            className='h-[100vh] bg-gradient-to-b from-red-50 to-white flex justify-center items-center flex-col'
             name='contact'
         >
             <div className='text-7xl h-auto text-red-400 flex justify-center items-center font-signature min-w-max pt-52 md:pt-28'>
                 Contact Me
             </div>
-            <div className='container'>
-                <form onSubmit={sendEmail}>
-                    <div className='flex flex-col pt-5 mx-auto'>
+            <div className='container flex items-center justify-center w-full'>
+                <form ref={form} onSubmit={sendEmail}>
+                    <div className='flex flex-col pt-5 mx-auto justify-center items-center w-full'>
                         <div className=' pt-2 mx-auto '>
                             <input
                                 type='text'
@@ -81,6 +112,10 @@ const ContactMe = () => {
                         <div className='pt-2 mx-auto'>{sendButton()}</div>
                     </div>
                 </form>
+            </div>
+            <div className='flex items-center justify-center pt-10'>
+                {' '}
+                KishanYerneni@gmail.com | (+1) 512-680-1441{' '}
             </div>
         </section>
     );
